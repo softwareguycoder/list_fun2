633d525df999a4deb3bbdd723e4131d767379b03
softwareguycoder <softwareguycoder@gmail.com>
2019-05-06 11:03:53 -0400

    Finished re-write to depend on list_core library for linked list

--- list_fun2/file.h
+++ list_fun2/file.h
@@ -4,7 +4,22 @@
 #ifndef __FILE_H__
 #define __FILE_H__
 
+/**
+ * @name OpenFile
+ * @brief Opens the file with the path name specified for reading.
+ * @param pszPathName String containing the path to the file to be opened.
+ * @param ppFile Address of a FILE pointer to be filled with the pointer to
+ * the newly-opened file, or with NULL if the operation failed.
+ * @remarks If the operation fails, the value ppFile points to
+ * is set to NULL.
+ */
 void OpenFile(const char* pszPathName, FILE** ppFile);
+
+/**
+ * @name CloseFile
+ * @brief Attempts to close the file specified.
+ * @param pFile Pointer to the file you want to close.
+ */
 void CloseFile(FILE* pFile);
 
 #endif /* __FILE_H__ */
