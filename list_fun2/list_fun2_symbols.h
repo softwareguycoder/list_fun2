d2954ebbe6fecb08b1b7ffea6bdd919e32b585b4
softwareguycoder <softwareguycoder@gmail.com>
2019-05-06 11:06:37 -0400

    Added FILE_OPENED_OK symbol

--- list_fun2/list_fun2_symbols.h
+++ list_fun2/list_fun2_symbols.h
@@ -9,4 +9,9 @@
   "Failed to open file '%s'.\n"
 #endif //FAILED_OPEN_FILE
 
+#ifndef FILE_OPENED_OK
+#define FILE_OPENED_OK \
+  "The file '%s' was opened successfully.\n"
+#endif //FILE_OPENED_OK
+
 #endif /* __LIST_FUN2_SYMBOLS_H__ */
